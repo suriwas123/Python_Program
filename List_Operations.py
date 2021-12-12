09062df62d1ec2f6db9cc32736c22573dca5599f
swasa1329 <52796476+swasa1329@users.noreply.github.com>
2021-12-12 12:27:13 +0530

    Create List_Operations.py

---
+++ List_Operations.py
@@ -0,0 +1,13 @@
+import os
+
+List1=[8,9,3,6,1,10]
+List1.reverse()
+print("The reversed list is:-",List1)
+
+List2=[91,64,120,45,72,8,76,23,100,67]
+List2.sort()
+print("The sorted list is :-",List2)
+
+List3=[]
+List3=List1.copy()
+print("Copied list is:-",List3)
